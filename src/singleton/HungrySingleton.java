4cddebaa0e8b844b52d7d397be135b7cbe9f6f30
bomb <qingqi_lei@icloud.com>
2018-09-11 08:10:56 +0800

    static inner class singleton

---
+++ src/singleton/HungrySingleton.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class HungrySingleton {
+    private final static HungrySingleton INSTANCE = new  HungrySingleton();
+
+    private HungrySingleton(){
+        if(INSTANCE != null){
+            throw new RuntimeException("此类为单例模式，已经被实例化了");
+        }
+    }
+
+    public static HungrySingleton getInstance(){
+        return INSTANCE;
+    }
+}
