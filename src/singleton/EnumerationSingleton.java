f7810498a0868fe07460e457102ed321e3fe3bb8
bomb <qingqi_lei@icloud.com>
2018-09-11 08:09:59 +0800

    double check and synchronized

---
+++ src/singleton/EnumerationSingleton.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public enum EnumerationSingleton {
+    INSTANCE;
+
+    public EnumerationSingleton getSingleton() {
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        EnumerationSingleton i = INSTANCE.getSingleton();
+        System.out.println(i.ordinal());
+        System.out.println(i.toString());
+        System.out.println(i.getDeclaringClass());
+    }
+}
